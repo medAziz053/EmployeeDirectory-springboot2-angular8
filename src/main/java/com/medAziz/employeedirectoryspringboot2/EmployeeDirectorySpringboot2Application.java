ad7a94b201da137743e623ac8fbe709f1ad6bdfa
medAziz <medaziz.ridene@gmail.com>
2019-09-22 01:01:36 +0100

    initial commit: set up spring boot 2 application

---
+++ src/main/java/com/medAziz/employeedirectoryspringboot2/EmployeeDirectorySpringboot2Application.java
@@ -0,0 +1,13 @@
+package com.medAziz.employeedirectoryspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDirectorySpringboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDirectorySpringboot2Application.class, args);
+	}
+
+}
